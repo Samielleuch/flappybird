821db95c07a328dcdcfaec929b8dd44b88487ad2
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2019-09-16 14:46:17 +0100

    added Game Template

--- src/assets/Game.js
+++ src/assets/Game.js
@@ -1,5 +1,23 @@
 export default function() {
   const canvas = document.querySelector("canvas");
-  // eslint-disable-next-line no-unused-vars
   const ctx = canvas.getContext("2d");
+  function update() {
+    //updates here
+  }
+
+  function draw() {
+    //draw here
+  }
+
+  function animate() {
+    //reset the canvas
+    ctx.fillStyle = "rgba(12, 20, 63, 0.25)";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    update();
+    draw();
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 }
